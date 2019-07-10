bf911a3aa6928c1642ba7c925643b4e6462910a2
H5307-100 <H5307-100@h5307-053.cloud.local>
2019-07-10 17:59:47 +0800

    copyright

--- ssm-parent/ssm-api/src/main/java/cn/edu/cuit/ssm/service/GoodsService.java
+++ ssm-parent/ssm-api/src/main/java/cn/edu/cuit/ssm/service/GoodsService.java
@@ -4,7 +4,13 @@ package cn.edu.cuit.ssm.service;
 import com.github.pagehelper.PageInfo;
 
 import cn.edu.cuit.ssm.entity.Goods;
-
+/**
+ * 
+ *  南阳德刚版权所有http://www.guodexian.com<br>
+ *
+ *
+ * 2019年7月10日下午5:57:51
+ */
 public interface GoodsService {
 
 	PageInfo<Goods> findAll(int pageNum,int pageSize);
--- ssm-parent/ssm-api/src/main/java/cn/edu/cuit/ssm/service/UserService.java
+++ ssm-parent/ssm-api/src/main/java/cn/edu/cuit/ssm/service/UserService.java
@@ -1,5 +1,12 @@
 package cn.edu.cuit.ssm.service;
-
+/**
+ * 
+ *  南阳德刚版权所有http://www.guodexian.com<br>
+ *
+ *
+ * 2019年7月10日下午5:58:17<br>
+ * 类说明:
+ */
 public interface UserService {
 
 	boolean login(String name, String pass);
