d1e7b55290e3f4411d27e9d0971ebbc3475a0d0f
H5307-100 <H5307-100@h5307-079.cloud.local>
2019-07-11 09:53:21 +0800

    cc

--- ssm-parent/ssm-service/src/main/java/cn/edu/cuit/ssm/service/impl/GoodsServiceImpl.java
+++ ssm-parent/ssm-service/src/main/java/cn/edu/cuit/ssm/service/impl/GoodsServiceImpl.java
@@ -2,6 +2,7 @@ package cn.edu.cuit.ssm.service.impl;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,13 @@ import com.github.pagehelper.PageInfo;
 import cn.edu.cuit.ssm.entity.Goods;
 import cn.edu.cuit.ssm.mapper.GoodsMapper;
 import cn.edu.cuit.ssm.service.GoodsService;
+/**
+ * 
+ * 南阳德刚版权所有<br>
+ * 个人主页http://www.guodexian.com<br>
+ * 2019年7月11日上午9:51:33
+ * 说明:注意修改类的注解
+ */
 @Service("goodsService")
 public class GoodsServiceImpl implements GoodsService {
 	@Autowired
--- ssm-parent/ssm-service/src/main/java/cn/edu/cuit/ssm/service/impl/UserServiceImpl.java
+++ ssm-parent/ssm-service/src/main/java/cn/edu/cuit/ssm/service/impl/UserServiceImpl.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Service;
 
 import cn.edu.cuit.ssm.mapper.UserMapper;
 import cn.edu.cuit.ssm.service.UserService;
-
+/**
+ * 
+ * 南阳德刚版权所有<br>
+ * 个人主页http://www.guodexian.com<br>
+ * 2019年7月11日上午9:52:32
+ * 说明:注意修改注解
+ */
 @Service("userService")
 public class UserServiceImpl implements UserService {
 	@Autowired
